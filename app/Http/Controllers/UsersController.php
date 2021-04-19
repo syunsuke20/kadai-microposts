44277fbddb79719d79253895996d635a99c228ee
syunsuke.nakanishi <t0h1a2n3k4y5o6u@icloud.com>
2021-04-19 03:53:44 +0000

    follow,unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -36,5 +36,26 @@ class UsersController extends Controller
         ]);
     }
     
+    public function followings($id)
+    {
+        $user =User::findOrFail($id);
+        $user->loadRelationshipCounts();
+        $followings = $user->followings()->paginate(10);
+        return view('users.followings',[
+            'user' =>$user,
+            'users' => $followings,
+            ]);
+    }
+    
+    public function followers($id)
+    {
+        $user = User::findOrFail($id);
+        $user->loadRelationshipCounts();
+        $followers = $user->followers()->paginate(10);
+        return view('users.followers',[
+            'user' => $user,
+            'users' =>$followers,
+            ]);
+    }
   
 }
